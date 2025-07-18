c54678ce0e08a690a00b04ccfe37ed812c3e8f93
Delphine Azodo <Andyazodo@gmail.com>
2025-07-18 13:19:37 -0100

    commit

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class User(db.Model,UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 class FormEntry(db.Model):
     id =  db.Column(db.Integer, primary_key=True)
     fullname = db.Column(db.String(120), nullable=False)
